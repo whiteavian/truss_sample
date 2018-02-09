e3aecb91dd4a2e4c7660211f8490f2251a5e0e9e
Laura White-Avian <whiteavian@users.noreply.github.com>
2018-02-08 17:39:59 -0800

    Modify process_line and initialize Column child classes.

--- normalize_csv.py
+++ normalize_csv.py
@@ -1,5 +1,7 @@
 import sys
 
+ADDRESS = 'Address'
+
 
 class CSV:
     def __init__(self):
@@ -11,46 +13,29 @@ class CSV:
         self.headers = process_headers(raw_input())
 
         for line in sys.stdin:
-            self.rows.append(process_line(line.strip()))
+            self.rows.append(self.process_line(line.strip()))
+
+    def address_index(self):
+        return self.headers.index(ADDRESS)
+
+    def process_line(self, line):
+        ai = self.address_index()
+        if ai == 0:
+            end_quote = line.find('"', 2)
+            second_split = line.split(',', end_quote + 1)
+        elif ai == len(self.headers) - 1:
+
+        first_split = line.split(self.address_index())
+        # TODO put quoted string checks and balances in place.
+        to_split = first_split[-1]
+
+        return first_split[:-1] + second_split
 
 
 def process_headers(header_line):
     return header_line.split(',')
 
 
-def process_line(line):
-    line_items = line.split('"')
-    # also do this for single quotes
-    num_items = len(line_items)
-
-    # If our csv contains only a single column, which is quoted, return it.
-    if num_items == 3 and line_items[0] == '' and line_items[2] == '':
-        return [line_items[1]]
-
-    split_line = []
-
-    # Otherwise, split columns by comma, accounting for quoted fields.
-    for i in range(num_items):
-        if i != 0 and line_items[i - 1].endswith(','):
-            prev_has_comma = True
-        else:
-            prev_has_comma = False
-
-        if i != num_items - 1 and line_items[i + 1].startswith(','):
-            next_has_comma = True
-        else:
-            next_has_comma = False
-
-        quoted = prev_has_comma and next_has_comma
-        quoted = quoted or (i == 0 and next_has_comma)
-        quoted = quoted or (i == num_items - 1 and prev_has_comma)
-
-        if not quoted:
-            split_line.extend(line_items[i].split(','))
-        else:
-            split_line.append(line_items[i])
-
-    return split_line
 
 
 class Row:
@@ -59,38 +44,39 @@ class Row:
 
 
 class Column:
-    def __init__(self):
-        pass
+    def __init__(self, text):
+        self.original_text = text
+        self.normalized_text = ''
 
 
 class TimestampColumn(Column):
-    def __init__(self):
-        pass
+    def __init__(self, *args):
+        super(TimestampColumn, self).__init__(*args)
 
 
 class ZipColumn(Column):
-    def __init__(self):
-        pass
+    def __init__(self, *args):
+        super(ZipColumn, self).__init__(*args)
 
 
 class AddressColumn(Column):
-    def __init__(self):
-        pass
+    def __init__(self, *args):
+        super(AddressColumn, self).__init__(*args)
 
 
 class FooBarDurationColumn(Column):
-    def __init__(self):
-        pass
+    def __init__(self, *args):
+        super(FooBarDurationColumn, self).__init__(*args)
 
 
 class TotalDurationColumn(Column):
-    def __init__(self):
-        pass
+    def __init__(self, *args):
+        super(TotalDurationColumn, self).__init__(*args)
 
 
 class NotesColumn(Column):
-    def __init__(self):
-        pass
+    def __init__(self, *args):
+        super(NotesColumn, self).__init__(*args)
 
 
 csv = CSV()
